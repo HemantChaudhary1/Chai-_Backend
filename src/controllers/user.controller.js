d79a23d51a92c79f7e2e5126ac5665d121a273dd
Hemant <hemuchoudhary231@gmail.com>
2024-03-09 09:40:40 +0530

    Add Register Controllers and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,89 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js" 
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req,res) =>{
+          //step1: get user details from frontend
+          //step2:-Validation like not empty,email correct syntax
+          //step3:-Check if user already exists (We can check using username or email)
+          //step4:-check for required files --> Here check for images and avatar
+          //step5:-upload them to cloudinary -->check if avatar is uploaded or not successfully
+          //step6:-create user object --> Create entry in db
+          //step7:-remove password and refresh field from response
+          //step8:-check for user creation --> if we have got null response or user have been created
+          //step9:-return response
 
-       res.status(200).json({
-        message:"chai aur code"
-      })
+     //***step1: get user details from frontend
+          const {username,email,password,fullName} =req.body
+          console.log("email:",email);
+
+    //***step2:-Validation like not empty,email correct syntax
+        //   if(fullName === ""){
+        //     throw new ApiError(400,"fullname is required")
+        //   }
+     //check for fields are empty or not
+        if(
+           [fullName,email,username,password].some( (field) => field?.trim() === "") 
+        ){
+             throw new ApiError(400,"All fields are required")
+        }
+      //check for email format is correct or not
+  
+  //***step3:-Check if user already exists (We can check using username or email)
+      const existedUser =  User.findOne({
+         $or:[{ username }, { email }]
+       })
+       
+       if(existedUser){
+        throw new ApiError(409,"User with username and email already exists")
+       }
+    
+   //***step4:-check for required files --> Here check for images and avatar
+      const avatarLocalPath = req.files?.avatar[0]?.path;  // this we get using multer and middleware which we have written before registerUser route
+      const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+       //check for avatar because it is required
+       if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required");
+       }
+
+   //***step5:-upload them to cloudinary -->check if avatar is uploaded or not successfully
+      const avatar = await uploadOnCloudinary(avatarLocalPath)
+      const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+       //check if avatar is uploaded or not successfully
+       if(!avatar){
+        throw new ApiError(400,"Avatar file is required");
+       }
+   
+        
+   //***step6:-create user object --> Create entry in db
+      const user = await User.create({
+         fullName,
+         avatar: avatar.url,
+         coverImage: coverImage?.url || "",
+         email,
+         password,
+         username: username.toLowerCase()
+       })
+       
+      //remove password and refresh field from response
+         const createdUser= await User.findById(user._id).select(       // In select all fields are by default selected , we use "-fieldName" to remove the field which we do not want to send in user response
+            "-password -refreshToken"
+         ) 
+     //check for user creation --> if we have got null response or user have been created
+        if(!createdUser){
+            throw new ApiError(500,"Something went wrong while registering the user")
+        }
+    
+  //***step9:-return response
+       return res.status(201).json(
+            new ApiResponse(200, createdUser, "User registered successfully")
+       )
+          
+
 } )
 
 export {registerUser,}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,22 @@
 // Now We will make Router and Routes 
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([             // Middleware is injected here before registerUser
+       {name:"avatar",
+       maxCount:1
+       },
+       {
+          name:"coverImage",
+          maxCount:1
+       }
+    ]),
+    registerUser
+    )
 
 
 export default router
